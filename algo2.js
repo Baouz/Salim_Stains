b0d3e95ebae62f21b4c2fcb54a50e5a089ccb277
baouz salim <baouz.salim93240@gmail.com>
2019-10-23 12:48:38 +0200

    nouvelle solution algo 2

--- algo2.js
+++ algo2.js
@@ -1,39 +1,34 @@
-/* 
-* Corriger la fonction minMax()
-* La fonction récupère un tableau de nombres positifs, négatifs ou nuls
-* Et retourne un tableau à 2 entrées contenant la valeur minimale
-* et la valeur maximale du tableau récupéré en entrée
-* 
-* Exemple 1 :
-* Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
-* Résultat : [-65, 67]
-* 
-* Exemple 2 :
-* Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
-* Résultat : [-30, 108]
-* 
-* Exemple 3 :
-* Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
-* Résultat : [7, 85]
-*/
-let result=[];
-let exemple= [56, 7, 63, 9, 7, 12, 85];
-
-let minMax = (newArr,array) => {
-    let min = array[0];
-    let max = array[0];
-  
-    for (let i = 0; i <array.length; i++) {
-      if (array[i] < min || array[i] === min) {
-        min = array[i];
-      }
-      if (array[i] > max || array[i] === max ) {
-        max = array[i];
-      }
-    };
-    newArr.push(min, max);
-    console.log(newArr);
+/*
+ * Corriger la fonction minMax()
+ * La fonction récupère un tableau de nombres positifs, négatifs ou nuls
+ * Et retourne un tableau à 2 entrées contenant la valeur minimale
+ * et la valeur maximale du tableau récupéré en entrée
+ *
+ * Exemple 1 :
+ * Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
+ * Résultat : [-65, 67]
+ *
+ * Exemple 2 :
+ * Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
+ * Résultat : [-30, 108]
+ *
+ * Exemple 3 :
+ * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
+ * Résultat : [7, 85]
+ */
+let minMax = array => {
+  let newArray = [];
+  let min = array[0];
+  let max = array[0];
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+    if (array[i] > max) {
+      max = array[i];
+    }
   }
-
-  minMax(result,exemple)
-  
+  newArray.push(min, max);
+  return newArray;
+};
+console.log(minMax([4, 4, 56, -5, 40, 23], [2, 3000]));
